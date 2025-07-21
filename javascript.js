40f57c8582289eb33f7dd240c99301dd58a2eb4f
Rodrigo <rodrigomareque8@gmail.com>
2025-07-21 17:52:33 -0300

    media query e javascript do header

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById('openMenu').onclick = function() {
+  document.getElementById('mobileMenu').classList.add('open');
+};
+document.getElementById('closeMenu').onclick = function() {
+  document.getElementById('mobileMenu').classList.remove('open');
+};
